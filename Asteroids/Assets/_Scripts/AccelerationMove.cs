c22918c08380feb3747e0a439b699dde000f4a83
Skorpion-tracer <Skorpion-tracer@yandex.ru>
2021-08-26 00:49:00 +0300

    HW Lesson 2.

---
+++ Asteroids/Assets/_Scripts/AccelerationMove.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Asteroids
+{
+    internal sealed class AccelerationMove : MoveTransform
+    {
+        private readonly float _acceleration;
+
+        public AccelerationMove(Rigidbody2D transform, float speed, float acceleration) : 
+            base(transform, speed)
+        {
+            _acceleration = acceleration;
+        }
+
+        public void AddAcceleration()
+        {
+            Speed += _acceleration;
+        }
+
+        public void RemoveAcceleration()
+        {
+            Speed -= _acceleration;
+        }
+    }
+}
---
+++ Asteroids/Assets/_Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    internal sealed class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _acceleration;
+        [SerializeField] private float _hp;
+        [SerializeField] private Rigidbody2D _rigidbodySpaceShip;
+        [SerializeField] private Rigidbody2D _bullet;
+        [SerializeField] private Transform _barrel;
+        [SerializeField] private float _force;
+        private Camera _camera;
+        private Ship _ship;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            var moveTransform = new AccelerationMove(_rigidbodySpaceShip, _speed, _acceleration);
+            var rotation = new RotationShip(transform);
+            var shoter = new Shooter(_bullet, _barrel, _force);
+            _ship = new Ship(moveTransform, rotation, shoter);
+        }
+
+        private void Update()
+        {
+            var direction = Input.mousePosition -
+                _camera.WorldToScreenPoint(transform.position);
+            _ship.Rotation(direction);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _ship.AddAcceleration();
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                _ship.RemoveAcceleration();
+            }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _ship.Shoot();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            _ship.Move(transform, Input.anyKey, Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (_hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _hp--;
+            }
+        }
+    }
+}
